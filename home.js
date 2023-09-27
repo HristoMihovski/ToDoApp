a71ea4ebf7adb1c8fea9a31a758a332cf5d76dd7
Hristo <144589040+HristoMihovski@users.noreply.github.com>
2023-09-27 20:43:21 +0300

    Update home.js

--- home.js
+++ home.js
@@ -1,98 +1,112 @@
-// app.js
-window.onload = () => {
-	const form1 = document.querySelector("#addForm");
+// On app load, get all tasks from localStorage
+window.onload = loadTasks;
 
-	let items = document.getElementById("items");
-	let submit = document.getElementById("submit");
+// On form submit add task
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+  addTask();
+});
 
-	let editItem = null;
+function loadTasks() {
+  // check if localStorage has any tasks
+  // if not then return
+  if (localStorage.getItem("tasks") == null) return;
 
-	form1.addEventListener("submit", addItem);
-	items.addEventListener("click", removeItem);
-};
+  // Get the tasks from localStorage and convert it to an array
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
 
-function addItem(e) {
-	e.preventDefault();
-
-	if (submit.value != "Submit") {
-		console.log("Hello");
-
-		editItem.target.parentNode.childNodes[0].data
-			= document.getElementById("item").value;
-
-		submit.value = "Submit";
-		document.getElementById("item").value = "";
-
-		document.getElementById("lblsuccess").innerHTML
-			= "Text edited successfully";
-
-		document.getElementById("lblsuccess")
-						.style.display = "block";
-
-		setTimeout(function() {
-			document.getElementById("lblsuccess")
-							.style.display = "none";
-		}, 3000);
-
-		return false;
-	}
-
-	let newItem = document.getElementById("item").value;
-	if (newItem.trim() == "" || newItem.trim() == null)
-		return false;
-	else
-		document.getElementById("item").value = "";
-
-	let li = document.createElement("li");
-	li.className = "list-group-item";
-
-	let deleteButton = document.createElement("button");
-
-	deleteButton.className =
-		"btn-danger btn btn-sm float-right delete";
-
-	deleteButton.appendChild(document.createTextNode("Delete"));
-
-	let editButton = document.createElement("button");
-
-	editButton.className =
-			"btn-success btn btn-sm float-right edit";
-
-	editButton.appendChild(document.createTextNode("Edit"));
-
-	li.appendChild(document.createTextNode(newItem));
-	li.appendChild(deleteButton);
-	li.appendChild(editButton);
-
-	items.appendChild(li);
+  // Loop through the tasks and add them to the list
+  tasks.forEach(task => {
+    const list = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? "checked" : ""}>
+          <input type="text" value="${task.task}" class="task ${task.completed ? "completed" : ""}" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+          <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+    list.insertBefore(li, list.children[0]);
+  });
 }
 
-function removeItem(e) {
-	e.preventDefault();
-	if (e.target.classList.contains("delete")) {
-		if (confirm("Are you Sure?")) {
-			let li = e.target.parentNode;
-			items.removeChild(li);
-			document.getElementById("lblsuccess").innerHTML
-				= "Text deleted successfully";
+function addTask() {
+  const task = document.querySelector("form input");
+  const list = document.querySelector("ul");
+  // return if task is empty
+  if (task.value === "") {
+    alert("Please add some task!");
+    return false;
+  }
+  // check is task already exist
+  if (document.querySelector(`input[value="${task.value}"]`)) {
+    alert("Task already exist!");
+    return false;
+  }
 
-			document.getElementById("lblsuccess")
-						.style.display = "block";
+  // add task to local storage
+  localStorage.setItem("tasks", JSON.stringify([...JSON.parse(localStorage.getItem("tasks") || "[]"), { task: task.value, completed: false }]));
 
-			setTimeout(function() {
-				document.getElementById("lblsuccess")
-						.style.display = "none";
-			}, 3000);
-		}
-	}
-	if (e.target.classList.contains("edit")) {
-		document.getElementById("item").value =
-			e.target.parentNode.childNodes[0].data;
-		submit.value = "EDIT";
-		editItem = e;
-	}
+  // create list item, add innerHTML and append to ul
+  const li = document.createElement("li");
+  li.innerHTML = `<input type="checkbox" onclick="taskComplete(this)" class="check">
+      <input type="text" value="${task.value}" class="task" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+      <i class="fa fa-trash" onclick="removeTask(this)"></i>`;
+  list.insertBefore(li, list.children[0]);
+  // clear input
+  task.value = "";
 }
 
-function toggleButton(ref, btnID) {
-	document.getElementById(btnID).disabled = false;
+function taskComplete(event) {
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  tasks.forEach(task => {
+    if (task.task === event.nextElementSibling.value) {
+      task.completed = !task.completed;
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  event.nextElementSibling.classList.toggle("completed");
+}
+
+function removeTask(event) {
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  tasks.forEach(task => {
+    if (task.task === event.parentNode.children[1].value) {
+      // delete task
+      tasks.splice(tasks.indexOf(task), 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  event.parentElement.remove();
+}
+
+// store current task to track changes
+var currentTask = null;
+
+// get current task
+function getCurrentTask(event) {
+  currentTask = event.value;
+}
+
+// edit the task and update local storage
+function editTask(event) {
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  // check if task is empty
+  if (event.value === "") {
+    alert("Task is empty!");
+    event.value = currentTask;
+    return;
+  }
+  // task already exist
+  tasks.forEach(task => {
+    if (task.task === event.value) {
+      alert("Task already exist!");
+      event.value = currentTask;
+      return;
+    }
+  });
+  // update task
+  tasks.forEach(task => {
+    if (task.task === currentTask) {
+      task.task = event.value;
+    }
+  });
+  // update local storage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
